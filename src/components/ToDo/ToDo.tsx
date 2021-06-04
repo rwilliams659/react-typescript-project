8328bf23c7996486509bb8614cd3f8b52fce1ee7
Rachel Williams <rachelwilliams659@gmail.com>
2021-06-04 12:52:15 -0600

    Add checkboxes to mark todo complete

--- src/components/ToDo/ToDo.tsx
+++ src/components/ToDo/ToDo.tsx
@@ -1,7 +1,15 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styles from './ToDo.module.scss'
 
 const ToDo = (props: { title: string, text: string, id: number, deleteTodo: (toDoId: number) => any }) => {
+
+const [completed, setCompleted] = useState<boolean>(false)
+
+const handleChecked = () => {
+  const newValue = !completed
+  setCompleted(newValue)
+}
+
   return (
     <section className={styles.todo}>
       <button className={styles.deleteBtn} onClick={() => props.deleteTodo(props.id)}>X</button>
@@ -9,6 +17,10 @@ const ToDo = (props: { title: string, text: string, id: number, deleteTodo: (toD
         <h3>{props.title}</h3>
         <p>{props.text}</p>
       </section>
+      <div className={styles.completedLabel}>
+        <label htmlFor="completed">{completed ? 'Mark uncomplete' : 'Mark completed' }</label>
+        <input type="checkbox" name="completed" value="completed" id={styles.completedBox} onChange={handleChecked} checked={completed}/>
+      </div>
     </section>
   )
 }
